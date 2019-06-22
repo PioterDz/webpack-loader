f2e4cc40f6acb9034da30a5d1c7272a6d42bec95
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-22 09:38:04 +0200

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = (env) => {
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
     const enviroment = env || 'production';
 
     return {
@@ -10,6 +25,7 @@ module.exports = (env) => {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.' + enviroment + '.bundle.js'
         },
+        plugins: plugins,
         module: {
             rules: [
                 {
